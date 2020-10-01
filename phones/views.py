745505a6feb4fe304cdf32f3a1e05a9065f5c31d
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-10-01 19:31:35 +0300

    done

--- phones/views.py
+++ phones/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, redirect
 from phones.models import Phone
 
+
 def show_catalog(request):
     template = 'catalog.html'
 
+    sort = request.GET.get('sort')
     query_set = Phone.objects.all()
+
+    if sort == 'price_low':
+        query_set = Phone.objects.order_by('price')
+    elif sort == 'price_high':
+        query_set = Phone.objects.order_by('price').reverse()
+    elif sort == 'name':
+        query_set = Phone.objects.order_by('name')
+    elif not sort:
+        query_set = Phone.objects.all()
 
     context = {
         'phones': query_set
@@ -14,9 +25,18 @@ def show_catalog(request):
 
 def show_product(request, slug):
     template = 'product.html'
-    
-    context = {}
+    # phones = Phone.objects.all()
+
+    if slug:
+        phone = Phone.objects.get(slug__iexact=slug)
+    else:
+        return redirect('catalog')
+
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
+
 
 def home_view(request):
     return redirect('catalog')
